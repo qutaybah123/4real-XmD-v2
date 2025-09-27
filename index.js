8b35d0bb6453637ffeca49354bdc60debb1c8796
qutaybah123 <qutaybahmaurid@gmail.com>
2025-09-27 10:01:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -70,10 +70,10 @@ setInterval(() => {
     }
 }, 30_000) // check every 30 seconds
 
-let phoneNumber = "911234567890"
+let phoneNumber = "255624314178"
 let owner = JSON.parse(fs.readFileSync('./data/owner.json'))
 
-global.botname = "KNIGHT BOT"
+global.botname = "4real BOT"
 global.themeemoji = "•"
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
 const useMobile = process.argv.includes("--mobile")
