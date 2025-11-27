a4d69e1b742bf5a31c3488787f47ca26ffeac4b2
qutaybah123 <qutaybahmaurid@gmail.com>
2025-11-27 22:26:04 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,6 +11,7 @@ const settings = {
   description: "This is a bot for managing group commands and automating tasks.",
   version: "9.0.9",
   updateZipUrl: "https://github.com/qutaybah123/4real-XmD-v2/archive/refs/heads/main.zip",
+  botImageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbzLpxAyMMFyvSATmkrx_VXMki2k0LyNl7WIDglkASBQ&s=10", 
 };
 
 module.exports = settings;
