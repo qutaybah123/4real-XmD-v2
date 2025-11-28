8a1caf69d92dc5cbbc06da39ebac5005bdddf02b
qutaybah123 <qutaybahmaurid@gmail.com>
2025-11-28 16:40:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ global.OPENDEEPSEEKR1_KEY = "sk-or-v1-a76b110cfe188e85835c007e957964fdce02d911b7
 global.OPENMISTRAL_KEY = "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";
 global.OPENMETA_KEY = "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";
 global.OPENANTHROPIC_KEY = "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";
-global.OPENNVIDIA_KEY = "sk-or-v1-0f02e2597ec48c51fe5163f0e6d5a0c23dff2041ef9edb5e0cdc55b7260356c";
+global.OPENNVIDIA_KEY = "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";
 global.OPENAI_API_KEY = "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"
 
 // Import lightweight store
